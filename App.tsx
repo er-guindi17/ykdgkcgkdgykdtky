3edf01f60690782af9c365b4f578bc8a278ea8fc
hbhdhedhdthh <fortnitale@gmail.com>
2025-10-11 03:41:37 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import React, { useState, useEffect } from 'react';
 import type { Playlist, SpotifyUserProfile } from './types';
 import { generatePlaylist } from './services/geminiService';
